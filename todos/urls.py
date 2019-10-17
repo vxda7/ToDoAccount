fecb14620c957fcfe00f17b83f23ba1d8493373d
vxda77 <vxda77@gmail.com>
2019-10-17 17:52:29 +0900

    finish

--- todos/urls.py
+++ todos/urls.py
@@ -19,5 +19,7 @@ from . import views
 app_name = 'todos'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:id>/delete', views.delete, name='delete'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from IPython import embed
-# Create your views here.
+from .forms import TodoForm
+from .models import Todo
+from django.contrib.auth.decorators import login_required
+# Create your views here
+
+
+@login_required
 def index(request):
     # session 사용해 봄
     # visit_num = request.session.get('visit_num', 0)
@@ -10,4 +16,34 @@ def index(request):
     # context = {
     #     'visit_num': visit_num
     # }
-    return render(request, 'todos/index.html')
+    
+    todos = request.user.todo_set.all().order_by('due_date')
+
+    context = {
+        'todos': todos
+    }
+
+    return render(request, 'todos/index.html', context)
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = request.user
+            todo.save()
+            return redirect('todos:index')
+    else:
+        form = TodoForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todos/form.html', context)
+
+@login_required
+def delete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if todo.user == request.user:
+        todo.delete()
+    return redirect('todos:index')
