9c6aa15c87a2e4e603bb1df6a800a74a69b66376
vxda77 <vxda77@gmail.com>
2019-10-17 10:02:26 +0900

    signup 1차 완성

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            user = form.save()
+            auth_login(request, user)
+            return redirect('todos:index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
